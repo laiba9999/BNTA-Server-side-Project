5c2688743eec792655de3defa7b1775b74813df0
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-12 15:55:10 +0000

    feat: Add business logic to createWorkplace() and createHouse()
    Add business logic to return exceptions if allotment being added to already has a building on it, or if allotment doesn't exist

--- src/main/java/com/teamname/buildings/workplaces/WorkplaceService.java
+++ src/main/java/com/teamname/buildings/workplaces/WorkplaceService.java
@@ -1,5 +1,8 @@
 package com.teamname.buildings.workplaces;
 
+import com.teamname.allotments.AllotmentService;
+import com.teamname.buildings.Building;
+import com.teamname.buildings.BuildingService;
 import com.teamname.exceptions.ResourcesNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +12,13 @@ import java.util.Optional;
 @Service
 public class WorkplaceService{
     private WorkplaceDAO workplaceDAO;
+    private BuildingService buildingService;
+    private AllotmentService allotmentService;
 
-
-    public WorkplaceService(WorkplaceDAO workplaceDAO) {
+    public WorkplaceService(WorkplaceDAO workplaceDAO, BuildingService buildingService, AllotmentService allotmentService) {
         this.workplaceDAO = workplaceDAO;
+        this.buildingService = buildingService;
+        this.allotmentService = allotmentService;
     }
 
     public List<Workplace> getAllWorkplace() {
@@ -27,19 +33,30 @@ public class WorkplaceService{
     }
 
     public void createWorkplace(Workplace workplace) {
+//        buildingService.getAllBuildings().forEach(p ->
+//            if(p.getAllotment_id()==workplace.getAllotment_id()){
+//
+//        })
+
+        for (Building building : buildingService.getAllBuildings()) {
+            if (building.getAllotment_id() == workplace.getAllotment_id()) {
+                throw new IllegalStateException("Allotment "+workplace.getAllotment_id()+" already has a building on it");
+            }
+            allotmentService.getAllotmentById(workplace.getAllotment_id());
+        }
         workplaceDAO.createWorkplace(workplace);
     }
 
     public void deleteWorkplace(int id){
         if(workplaceDAO.selectWorkplaceById(id).isEmpty()){
-            throw new ResourcesNotFoundException("Workplace with id: "+ id + "is not found");
+            throw new ResourcesNotFoundException("Workplace with id: "+ id + " is not found");
         }
         workplaceDAO.deleteWorkplace(id);
     }
 
     public void updateWorkplace(int id, Workplace workplace){
         if(workplaceDAO.selectWorkplaceById(id).isEmpty()){
-            throw new ResourcesNotFoundException("Workplace with id: "+ id + "is not found");
+            throw new ResourcesNotFoundException("Workplace with id: "+ id + " is not found");
         }
         workplaceDAO.updateWorkplace(id, workplace);
     }
