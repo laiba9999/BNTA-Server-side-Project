b6d80df4bd73d006d698915f9179cdb9ebb5da67
mechanin <86329519+mechanin@users.noreply.github.com>
2021-11-12 14:33:12 +0000

    Changes by jonathanhutchinson

--- src/main/java/com/teamname/citizens/Citizen.java
+++ src/main/java/com/teamname/citizens/Citizen.java
@@ -46,4 +46,27 @@ public class Citizen {
     public void setWorkplace_id(Integer workplace_id) {
         this.workplace_id = workplace_id;
     }
+
+    @Override
+    public String toString() {
+        return "Citizen{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", house_id=" + house_id +
+                ", workplace_id=" + workplace_id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Citizen citizen = (Citizen) o;
+        return Objects.equals(id, citizen.id) && Objects.equals(fullName, citizen.fullName) && Objects.equals(house_id, citizen.house_id) && Objects.equals(workplace_id, citizen.workplace_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, house_id, workplace_id);
+    }
 }
