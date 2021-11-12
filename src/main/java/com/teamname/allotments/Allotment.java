bee4bf0bbc4ede723af648270f446bd3833c4998
mechanin <86329519+mechanin@users.noreply.github.com>
2021-11-12 10:56:59 +0000

    FEATURE: Added x-y coordinate fields to the Allotment class and modified SQL database migration to reflect these changes

--- src/main/java/com/teamname/allotments/Allotment.java
+++ src/main/java/com/teamname/allotments/Allotment.java
@@ -2,9 +2,13 @@ package com.teamname.allotments;
 
 public class Allotment {
     private Integer id;
+    private Integer x_coordinate;
+    private String y_coordinate;
 
-    public Allotment(Integer id) {
+    public Allotment(Integer id, Integer x_coordinate, String y_coordinate) {
         this.id = id;
+        this.x_coordinate = x_coordinate;
+        this.y_coordinate = y_coordinate;
     }
 
     public Allotment() {
@@ -12,6 +16,22 @@ public class Allotment {
 
     public Integer getId() {
         return id;
+    }
+
+    public Integer getX_coordinate() {
+        return x_coordinate;
+    }
+
+    public void setX_coordinate(Integer x_coordinate) {
+        this.x_coordinate = x_coordinate;
+    }
+
+    public String getY_coordinate() {
+        return y_coordinate;
+    }
+
+    public void setY_coordinate(String y_coordinate) {
+        this.y_coordinate = y_coordinate;
     }
 
     public void setId(Integer id) {
--- src/main/resources/db/migration/V1__Initial_Set_Up.sql
+++ src/main/resources/db/migration/V1__Initial_Set_Up.sql
@@ -1,3 +1,5 @@
 CREATE TABLE allotments(
-    id SERIAL PRIMARY KEY NOT NULL
+    id SERIAL PRIMARY KEY NOT NULL,
+     x_coordinate INT,
+     y_coordinate VARCHAR(4)
     );
