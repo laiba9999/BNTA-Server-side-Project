396dcd3d0c7375a22eb978acde96dfa82b332c5f
laiba9999 <laibakirmani@hotmail.com>
2021-11-12 12:05:23 +0000

    Added exceptions to the workplace Service class

--- src/main/java/com/teamname/buildings/workplaces/WorkplaceDAO.java
+++ src/main/java/com/teamname/buildings/workplaces/WorkplaceDAO.java
@@ -1,12 +1,17 @@
 package com.teamname.buildings.workplaces;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface WorkplaceDAO {
     List<Workplace> selectAllWorkplace();
+
     Optional<Workplace> selectWorkplaceById(int id);
-    int createWorkplace (Workplace workplace);
-    int deleteWorkplace (int id);
-    int updateWorkplace (int id, Workplace workplace);
+
+    int createWorkplace(Workplace workplace);
+
+    int deleteWorkplace(int id);
+
+    int updateWorkplace(int id, Workplace workplace);
 }
