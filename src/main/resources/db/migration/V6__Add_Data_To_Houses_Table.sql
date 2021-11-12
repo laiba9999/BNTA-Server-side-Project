bd4d4190f3f158bba1a49aecc48e6d993c607621
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-12 16:40:43 +0000

    feat: Update data for SQL database
    Update data in allotments table to make a 5x5 grid and update data for houses, workplaces and citizens so that there are no errors/clashes

---
+++ src/main/resources/db/migration/V6__Add_Data_To_Houses_Table.sql
@@ -0,0 +1,5 @@
+INSERT INTO houses (buildingName, capacity, allotment_id) VALUES ('3 Mansfield Lane', 131, 1);
+INSERT INTO houses (buildingName, capacity, allotment_id) VALUES ('45 Hazelwood Avenue', 97, 2);
+INSERT INTO houses (buildingName, capacity, allotment_id) VALUES ('6 Valri Road', 153, 3);
+INSERT INTO houses (buildingName, capacity, allotment_id) VALUES ('67 Salop Road', 18, 4);
+INSERT INTO houses (buildingName, capacity, allotment_id) VALUES ('14 Thierer Avenue', 21, 5);
